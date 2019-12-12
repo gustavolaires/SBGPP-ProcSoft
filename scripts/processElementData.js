518f7dd970b9e068947accc710d6d33cac7a24e2
Gustavo Laires Albuquerque Costa <gustavolaires@hotmail.com>
2019-12-12 11:14:26 -0300

    Atualizacao - SBGPP_Processo de Software v2

--- scripts/processElementData.js
+++ scripts/processElementData.js
@@ -11,3 +11,42 @@
 
 // This file will include dynamically generated process layout related data from 
 // the publishing service 
+contentPage.processPage.imageFiles["_BV5SkRzZEeqEKd2Hz8HkwQ,_fF7M0RzZEeqEKd2Hz8HkwQ,_hYGq0xzZEeqEKd2Hz8HkwQActivity"]="./../../SBGPP/deliveryprocesses/resources/Atividades do Estágio_538AF0A0_cb8c613_Activity.jpeg"
+contentPage.processPage.imageFiles["_BV5SkRzZEeqEKd2Hz8HkwQ,_fF7M0RzZEeqEKd2Hz8HkwQ,_hYGq0RzZEeqEKd2Hz8HkwQActivity"]="./../../SBGPP/deliveryprocesses/resources/Atividades Pré-Estágio_5A53FC7A_1381d1ed_Activity.jpeg"
+contentPage.processPage.imageFiles["_BV5SkRzZEeqEKd2Hz8HkwQ,_fF7M0RzZEeqEKd2Hz8HkwQ,_hYGq1RzZEeqEKd2Hz8HkwQActivity"]="./../../SBGPP/deliveryprocesses/resources/Atividades Pós-Estágio_1EE5DE99_d813b40c_Activity.jpeg"
+contentPage.processPage.elementUrls["_K_DhBBzgEeqEKd2Hz8HkwQ"] = ["Gerente do jogo","SBGPP/deliveryprocesses/gerente_do_jogo_CEA91202.html"];
+contentPage.processPage.elementUrls["_y0zLNRzZEeqEKd2Hz8HkwQ"] = ["Quadro de Aspectos","SBGPP/deliveryprocesses/quadro_de_aspectos_B96C230A.html"];
+contentPage.processPage.elementUrls["_tgTywRzZEeqEKd2Hz8HkwQ"] = ["Estimando a Lista de Aspectos Primários","SBGPP/deliveryprocesses/estimando_a_lista_de_aspectos_primarios_DF6D070.html"];
+contentPage.processPage.elementUrls["_BBUUQhzaEeqEKd2Hz8HkwQ"] = ["Gerente de Processo","SBGPP/deliveryprocesses/gerente_de_processo_275A643B.html"];
+contentPage.processPage.elementUrls["_hYGq0xzZEeqEKd2Hz8HkwQ"] = ["Atividades do Estágio","SBGPP/deliveryprocesses/Atividades do Estágio_538AF0A0.html"];
+contentPage.processPage.elementUrls["_BBUUQRzaEeqEKd2Hz8HkwQ"] = ["Time de Mecânicas e Jogabilidade","SBGPP/deliveryprocesses/time_de_mecanicas_e_jogabilidade_38C15025.html"];
+contentPage.processPage.elementUrls["_y0gQQRzZEeqEKd2Hz8HkwQ"] = ["Retrospectiva do Estágio","SBGPP/deliveryprocesses/retrospectiva_do_estagio_DA72C91F.html"];
+contentPage.processPage.elementUrls["_tgTywxzZEeqEKd2Hz8HkwQ"] = ["Priorizando os Aspectos Primários","SBGPP/deliveryprocesses/priorizando_os_aspectos_primarios_72DC496.html"];
+contentPage.processPage.elementUrls["_tgTywhzZEeqEKd2Hz8HkwQ"] = ["Plano de Lançamentos","SBGPP/deliveryprocesses/plano_de_lancamentos_FC8FE486.html"];
+contentPage.processPage.elementUrls["_y0zLMxzZEeqEKd2Hz8HkwQ"] = ["Gerente do jogo","SBGPP/deliveryprocesses/gerente_do_jogo_EE113511.html"];
+contentPage.processPage.elementUrls["_hYGq0RzZEeqEKd2Hz8HkwQ"] = ["Atividades Pré-Estágio","SBGPP/deliveryprocesses/Atividades Pré-Estágio_5A53FC7A.html"];
+contentPage.processPage.elementUrls["_K_NSARzgEeqEKd2Hz8HkwQ"] = ["Idéias de Elementos do Estágio","SBGPP/deliveryprocesses/ideias_de_elementos_do_estagio_420D6AD4.html"];
+contentPage.processPage.elementUrls["_fF7M0RzZEeqEKd2Hz8HkwQ"] = ["iteracao","SBGPP/deliveryprocesses/iteracao_F7DB2995.html"];
+contentPage.processPage.elementUrls["_tgc8sRzZEeqEKd2Hz8HkwQ"] = ["Time de Mecânicas e Jogabilidade","SBGPP/deliveryprocesses/time_de_mecanicas_e_jogabilidade_55E80584.html"];
+contentPage.processPage.elementUrls["_tgc8sxzZEeqEKd2Hz8HkwQ"] = ["Gerente do jogo","SBGPP/deliveryprocesses/gerente_do_jogo_4F1EF9AA.html"];
+contentPage.processPage.elementUrls["_BBeFQBzaEeqEKd2Hz8HkwQ"] = ["Quadro de Aspectos","SBGPP/deliveryprocesses/quadro_de_aspectos_9B451BB6.html"];
+contentPage.processPage.elementUrls["_BBeFQRzaEeqEKd2Hz8HkwQ"] = ["Guia de Aspectos do Estágio","SBGPP/deliveryprocesses/guia_de_aspectos_do_estagio_A5E2FBC6.html"];
+contentPage.processPage.elementUrls["_K_DhABzgEeqEKd2Hz8HkwQ"] = ["Balanceamento do Estágio","SBGPP/deliveryprocesses/balanceamento_do_estagio_A172FE3.html"];
+contentPage.processPage.elementUrls["_K_NSABzgEeqEKd2Hz8HkwQ"] = ["Incremento de Jogo Jogável.","SBGPP/deliveryprocesses/incremento_de_jogo_jogavel_376F8AC4.html"];
+contentPage.processPage.elementUrls["_K_DhARzgEeqEKd2Hz8HkwQ"] = ["Construção do Estágio","SBGPP/deliveryprocesses/construcao_do_estagio_14B50FF3.html"];
+contentPage.processPage.elementUrls["_tgc8shzZEeqEKd2Hz8HkwQ"] = ["Gerente de Processo","SBGPP/deliveryprocesses/gerente_de_processo_4481199A.html"];
+contentPage.processPage.elementUrls["_BBUUQBzaEeqEKd2Hz8HkwQ"] = ["Time Audiovisual","SBGPP/deliveryprocesses/time_audiovisual_2E237015.html"];
+contentPage.processPage.elementUrls["_BV5SkRzZEeqEKd2Hz8HkwQ"] = ["ciclo","SBGPP/deliveryprocesses/ciclo_26F5FE36.html"];
+contentPage.processPage.elementUrls["_hYGq1RzZEeqEKd2Hz8HkwQ"] = ["Atividades Pós-Estágio","SBGPP/deliveryprocesses/Atividades Pós-Estágio_1EE5DE99.html"];
+contentPage.processPage.elementUrls["__fRSsBzZEeqEKd2Hz8HkwQ"] = ["Reunião de Planejamento do Estágio","SBGPP/deliveryprocesses/reuniao_de_planejamento_do_estagio_84F1F3F4.html"];
+contentPage.processPage.elementUrls["_y0zLMhzZEeqEKd2Hz8HkwQ"] = ["Gerente de Processo","SBGPP/deliveryprocesses/gerente_de_processo_E3735501.html"];
+contentPage.processPage.elementUrls["_y0zLNBzZEeqEKd2Hz8HkwQ"] = ["Guia de Aspectos do Estágio","SBGPP/deliveryprocesses/guia_de_aspectos_do_estagio_AECE42FA.html"];
+contentPage.processPage.elementUrls["_BA4PYBzaEeqEKd2Hz8HkwQ"] = ["Reunião Diária","SBGPP/deliveryprocesses/reuniao_diaria_65E90088.html"];
+contentPage.processPage.elementUrls["_K_DhAhzgEeqEKd2Hz8HkwQ"] = ["Reunião de BrainStorm","SBGPP/deliveryprocesses/reuniao_de_brainstorm_34E2409.html"];
+contentPage.processPage.elementUrls["_y0zLMBzZEeqEKd2Hz8HkwQ"] = ["Time Audiovisual","SBGPP/deliveryprocesses/time_audiovisual_EA3C60DB.html"];
+contentPage.processPage.elementUrls["_y0zLMRzZEeqEKd2Hz8HkwQ"] = ["Time de Mecânicas e Jogabilidade","SBGPP/deliveryprocesses/time_de_mecanicas_e_jogabilidade_F4DA40EB.html"];
+contentPage.processPage.elementUrls["_K_DhAxzgEeqEKd2Hz8HkwQ"] = ["Teste do Estágio","SBGPP/deliveryprocesses/teste_do_estagio_DEC0419.html"];
+contentPage.processPage.elementUrls["_y0gQQhzZEeqEKd2Hz8HkwQ"] = ["Reunião de Revisão do Estágio","SBGPP/deliveryprocesses/reuniao_de_revisao_do_estagio_C90BDD35.html"];
+contentPage.processPage.elementUrls["_tgc8sBzZEeqEKd2Hz8HkwQ"] = ["Time Audiovisual","SBGPP/deliveryprocesses/time_audiovisual_4B4A2574.html"];
+contentPage.processPage.elementUrls["__f3voBzZEeqEKd2Hz8HkwQ"] = ["Guia de Aspectos do Estágio","SBGPP/deliveryprocesses/guia_de_aspectos_do_estagio_845CD35A.html"];
+contentPage.processPage.elementUrls["_tgmtsBzZEeqEKd2Hz8HkwQ"] = ["Lista de Aspectos Primários","SBGPP/deliveryprocesses/lista_de_aspectos_primarios_8B8B6E66.html"];
